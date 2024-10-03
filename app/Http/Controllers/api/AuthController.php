a0809af05acf544211b3b1be98082f41e3acd529
pentekmate <90307397+pentekmate@users.noreply.github.com>
2024-10-03 10:50:15 +0200

    sync

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -30,8 +30,6 @@ class AuthController extends Controller
             ]);
         }
 
-
-
         $token = $user->createToken('api-token')->plainTextToken;
         $user->notify(
             new UserLoggedIn()
